93e1d03851223ae5ad328d34800fda0ce8cfab6a
parshimipopeli <plamorski@simplonprod.co>
2020-10-02 21:30:31 +0200

    Refactoring by adding Timestampable trait

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -2,8 +2,9 @@
 
 namespace App\Entity;
 
-use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\PinRepository;
+use App\Entity\Traits\Timestampable;
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
@@ -12,6 +13,10 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Pin
 {
+
+
+    use Timestampable;
+    
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -29,15 +34,6 @@ class Pin
      */
     private $description;
 
-    /**
-     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
-     */
-    private $createdAt;
-
-    /**
-     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
-     */
-    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -72,60 +68,5 @@ class Pin
 
    
 
-    /**
-     * @ORM\PrePersist
-     * @ORM\PreUpdate
-     */
-
-    public function updateTimestamps()
-    {
-
-        if ($this->getCreatedAt() === null) {
-        $this->setCreatedAt(new \DateTimeImmutable);
-
-            # code...
-        }
-        $this->setUpdatedAt(new \DateTimeImmutable);
-
-    }
-
-    /**
-     * Get the value of createdAt
-     */ 
-    public function getCreatedAt()
-    {
-        return $this->createdAt;
-    }
-
-    /**
-     * Set the value of createdAt
-     *
-     * @return  self
-     */ 
-    public function setCreatedAt($createdAt)
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of updatedAt
-     */ 
-    public function getUpdatedAt()
-    {
-        return $this->updatedAt;
-    }
-
-    /**
-     * Set the value of updatedAt
-     *
-     * @return  self
-     */ 
-    public function setUpdatedAt($updatedAt)
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
+   
 }
