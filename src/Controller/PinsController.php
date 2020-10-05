3e3660954600fa0e466bf142e7dc4ec87f353360
parshimipopeli <plamorski@simplonprod.co>
2020-10-05 17:43:10 +0200

    Add delete

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -90,7 +90,22 @@ return $this->render('pins/show.html.twig', compact('pin'));
                 'pin' => $pin,
                 'form' => $form->createView()
             ]);
+    }
 
+
+
+ /**
+     * @route("/pins/{id<[0-9]+>}/delete", name="app_pins_delete", methods="DELETE")
+     */
+
+public function delete(Pin $pin, EntityManagerInterface $em): Response
+{
+$em->remove($pin);
+$em->flush();
+
+            return $this->redirectToRoute('app_home');
 
 }
+
+
 }
