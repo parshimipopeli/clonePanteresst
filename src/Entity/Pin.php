597d3452cbcf44cd750588e37bcf53d76da91861
parshimipopeli <plamorski@simplonprod.co>
2020-10-06 20:16:25 +0200

    add bootstrap & use form themes

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -52,7 +52,7 @@ class Pin
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    public function setTitle(?string $title): self
     {
         $this->title = $title;
 
@@ -64,7 +64,7 @@ class Pin
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    public function setDescription(?string $description): self
     {
         $this->description = $description;
 
