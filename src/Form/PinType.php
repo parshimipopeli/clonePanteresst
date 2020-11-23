6777d4919967fc698905ecc87e42d6200d78bb55
parshimipopeli <plamorski@simplonprod.co>
2020-11-23 11:58:20 +0100

    add Textype and TextareaType and EmailType in forms

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PinType extends AbstractType
 {
@@ -22,8 +24,8 @@ class PinType extends AbstractType
                 'download_uri' => false,
                 'imagine_pattern' => 'squared_thumbnail_medium',
             ])
-            ->add('title')
-            ->add('description')
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
             ;
     }
 
