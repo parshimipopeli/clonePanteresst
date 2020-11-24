3d6df8d4c124b53267bb106de772bf688bb26c3d
parshimipopeli <plamorski@simplonprod.co>
2020-11-24 13:25:08 +0100

    Add knp_paginator and bootstrap pager navigation théme in home

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -10,15 +10,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface; // Nous appelons le bundle KNP Paginator
 
 class PinsController extends AbstractController
 {
     /**
      * @Route("/", name="app_home", methods="GET")
      */
-    public function index(PinRepository $pinRepository): Response
+    public function index(REquest $request, PinRepository $pinRepository, PaginatorInterface $paginator): Response
     {
         $pins = $pinRepository->findBy([], ['createdAt' => 'DESC']);
+
+        $pins = $paginator->paginate(
+            $pins, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            6 // Nombre de résultats par page
+        );
+        
 
         return $this->render('pins/index.html.twig', compact('pins'));
     }
